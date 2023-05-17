f6ff71c22f4c850a1845363bd36b74177825854f
Ajay Bhardwaj <ajaybhardwaj1999@gmail.com>
2023-05-18 00:36:56 +0530

    Redis List and Hash

--- src/main/java/com/project/redis/PersonConfig.java
+++ src/main/java/com/project/redis/PersonConfig.java
@@ -1,5 +1,6 @@
 package com.project.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class PersonConfig {
 
+    @Bean
     public LettuceConnectionFactory getConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis-10476.c301.ap-south-1-1.ec2.cloud.redislabs.com", 10476);
         redisStandaloneConfiguration.setPassword("hxe3ntWy32znIi8AZTep5RLmPlsbc1er");
@@ -19,14 +21,19 @@ public class PersonConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Person> getTemplate() {
-        RedisTemplate<String, Person> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, Object> getTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(getConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(getConnectionFactory());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
     }
 }
--- src/main/java/com/project/redis/PersonController.java
+++ src/main/java/com/project/redis/PersonController.java
@@ -1,25 +1,132 @@
 package com.project.redis;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
 public class PersonController {
+    private final String KEY_PREFIX = "per::";
+    private final String PERSON_LIST_KEY = "per_list";
+    private final String PERSON_HASH_KEY_PREFIX = "per_hash::";
+
     @Autowired
-    RedisTemplate<String, Person> redisTemplate;
+    ObjectMapper objectMapper;
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
 
     /**
      *   String ops
+     *   Key: String
+     *   Value: Object
      **/
     @PostMapping("/string/person")
     public void savePerson(@RequestBody Person person) {
-        String key = String.valueOf(person.getId());
+        if(person.getId() == 0) {
+            return;
+        }
+        String key = getKey(person.getId());
         redisTemplate.opsForValue().set(key, person);
     }
 
     @GetMapping("/string/person")
     public Person getPerson(@RequestParam("id") int id) {
-        return redisTemplate.opsForValue().get(id);
+        return (Person) redisTemplate.opsForValue().get(getKey(id));
+    }
+
+    private String getKey(long id) {
+        return KEY_PREFIX + String.valueOf(id);
+    }
+
+    /**
+     *   String ops
+     *   Key: String
+     *   Value: List<Object>
+     **/
+    @PostMapping("/lpush/person")
+    public void lpush(@RequestBody Person person) {
+        redisTemplate.opsForList().leftPush(PERSON_LIST_KEY, person);
+    }
+
+    @PostMapping("/rpush/person")
+    public void rpush(@RequestBody Person person) {
+        redisTemplate.opsForList().rightPush(PERSON_LIST_KEY, person);
+    }
+
+    /* Not working
+
+    @PostMapping("/lpush/person")
+    public void lpush(@RequestBody String person) throws Exception {
+        List<Person> personList = mapper.readValue(person, new TypeReference<List<Person>>() {});
+        redisTemplate.opsForList().leftPushAll(PERSON_LIST_KEY, personList);
+    }
+
+    @PostMapping("/rpush/person")
+    public void rpush(@RequestBody String person) throws Exception {
+        List<Person> personList = mapper.readValue(person, new TypeReference<List<Person>>() {});
+        redisTemplate.opsForList().rightPushAll(PERSON_LIST_KEY, personList);
+    }
+*/
+    @DeleteMapping("/lpop/person")
+    public List<Person> lpop(@RequestParam(value = "count", required = false, defaultValue = "1") int count) {
+        return redisTemplate.opsForList().leftPop(PERSON_LIST_KEY, count)
+                .stream()
+                .map(x -> (Person)x)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/rpop/person")
+    public List<Person> rpop(@RequestParam(value = "count", required = false, defaultValue = "1") int count) {
+        return redisTemplate.opsForList().rightPop(PERSON_LIST_KEY, count)
+                .stream()
+                .map(x -> (Person) x)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("lrange/person")
+    public List<Person> lrange(@RequestParam(value = "start", required = false, defaultValue = "0") int start,
+                               @RequestParam(value = "end", required = false, defaultValue = "-1") int end) {
+
+        return redisTemplate.opsForList().range(PERSON_LIST_KEY, start, end)
+                .stream()
+                .map(x -> (Person) x)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *   Hash ops
+     *   Key: String
+     *   Field: String
+     *   Value: String
+     **/
+    @PostMapping("/hash/person")
+    public void savePersonInHash(@RequestBody List<Person> people) {
+        people.stream()
+                .filter(person -> person.getId() != 0)
+                .forEach(person -> {
+                    Map map = objectMapper.convertValue(person, Map.class);
+                    redisTemplate.opsForHash().putAll(getHashKey(person.getId()), map);
+                    redisTemplate.expire(getHashKey(person.getId()), Duration.ofHours(24));
+                });
+    }
+
+    @GetMapping("/hash/person/all")
+    public List<Person> getPeople(@RequestParam("ids") List<Integer> peopleIds) {
+        return peopleIds.stream()
+                .map(i -> redisTemplate.opsForHash().entries((getHashKey(i))))
+                .map(entryMap -> objectMapper.convertValue(entryMap, Person.class))
+                .collect(Collectors.toList());
+    }
+
+    private String getHashKey(long id) {
+        return PERSON_HASH_KEY_PREFIX + String.valueOf(id);
     }
 }
